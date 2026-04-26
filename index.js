e9e5b980b2e1cb9b75f42f4498b4c769af6bad7d
jarexyvan-hub <jarexyvan@gmail.com>
2026-04-26 18:22:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.login(client.login(process.env.MTQ5Nzg3ODEyNDkyNTQyMzczNw.G7RNqz.5n2QQnLhhE-88LBtepszp8wkKx98I5eIcvfXAg);
+client.login(process.env.MTQ5Nzg3ODEyNDkyNTQyMzczNw.G7RNqz.5n2QQnLhhE-88LBtepszp8wkKx98I5eIcvfXAg);
 
